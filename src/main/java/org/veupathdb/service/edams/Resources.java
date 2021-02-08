9e31d29366433e7412f3ead69031f6db0a1809cd
Ryan Doherty <rdoherty@upenn.edu>
2021-02-08 16:48:56 -0500

    Initial commit of service template with schema

---
+++ src/main/java/org/veupathdb/service/edams/Resources.java
@@ -0,0 +1,53 @@
+package org.veupathdb.service.edams;
+
+import org.veupathdb.lib.container.jaxrs.config.Options;
+import org.veupathdb.lib.container.jaxrs.server.ContainerResources;
+import org.veupathdb.service.edams.service.MergeService;
+
+/**
+ * Service Resource Registration.
+ *
+ * This is where all the individual service specific resources and middleware
+ * should be registered.
+ */
+public class Resources extends ContainerResources {
+
+  private static final boolean DEVELOPMENT_MODE = true;
+
+  public static final String SUBSETTING_SERVICE_URL;
+
+  static {
+    SUBSETTING_SERVICE_URL = loadRequiredEnvVar("SUBSETTING_SERVICE_URL");
+  }
+
+  private static String loadRequiredEnvVar(String name) {
+    String value = System.getenv(name);
+    if (value == null) {
+      throw new RuntimeException("Required environment variable '" + name + "' is not defined.");
+    }
+    return value;
+  }
+
+  public static boolean logResponseHeadersAsClient() {
+    return DEVELOPMENT_MODE;
+  }
+
+  public Resources(Options opts) {
+    super(opts);
+    if (DEVELOPMENT_MODE) {
+      enableJerseyTrace();
+    }
+  }
+
+  /**
+   * Returns an array of JaxRS endpoints, providers, and contexts.
+   *
+   * Entries in the array can be either classes or instances.
+   */
+  @Override
+  protected Object[] resources() {
+    return new Object[] {
+      MergeService.class,
+    };
+  }
+}
