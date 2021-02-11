295330e52cf2dba88bc6fac286192bb341930e31
Ryan Doherty <rdoherty@upenn.edu>
2021-02-11 00:53:28 -0500

    Refactor to support universally named generated classes across EDA services

--- src/main/java/org/veupathdb/service/eda/ms/Main.java
+++ src/main/java/org/veupathdb/service/eda/ms/Main.java
@@ -1,4 +1,4 @@
-package org.veupathdb.service.edams;
+package org.veupathdb.service.eda.ms;
 
 import org.veupathdb.lib.container.jaxrs.config.Options;
 import org.veupathdb.lib.container.jaxrs.server.ContainerResources;
